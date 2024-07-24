62f961cb3b697bc0f0208f187d6d4b397f15b216
Mrunal00712 <117890321+Mrunal00712@users.noreply.github.com>
2024-07-24 15:56:08 +0530

    Update UserRepository.java

--- src/main/java/customlogin/repositories/UserRepository.java
+++ src/main/java/customlogin/repositories/UserRepository.java
@@ -1,14 +1,12 @@
 package customlogin.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import customlogin.model.User;
 
-
 @Repository
-public interface UserRepository  extends JpaRepository<User, Long>{
-	
-	User findByUsername(String username);
-
+public interface UserRepository extends MongoRepository<User, String> {
+    
+    User findByUsername(String username);
 }
